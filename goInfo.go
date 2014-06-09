c020e71e00905afee059d4c97e88bdee7f8246f0
matishsiao <matismaya@gmail.com>
2014-06-09 15:15:31 +0800

    add windows info

--- goInfo.go
+++ goInfo.go
@@ -5,18 +5,22 @@ import (
 )
 
 type GoInfoObject struct {
+	GoOS string
 	Kernel string
 	Core string
 	Platform string
 	OS string
 	Hostname string
+	CPUs int
 }
 
 func (gi *GoInfoObject) VarDump() {
+	fmt.Println("GoOS:",gi.GoOS)
 	fmt.Println("Kernel:",gi.Kernel)
 	fmt.Println("Core:",gi.Core)
 	fmt.Println("Platform:",gi.Platform)
 	fmt.Println("OS:",gi.OS)
 	fmt.Println("Hostname:",gi.Hostname)
+	fmt.Println("CPUs:",gi.CPUs)
 }
 
--- goInfo_windows.go
+++ goInfo_windows.go
@@ -5,11 +5,12 @@ import (
 	"os/exec"
 	"os"
 	"bytes"
+	"runtime"
 )
 
 func GetInfo() *GoInfoObject {			
-	cmd := exec.Command("cmd","SystemInfo")
-	cmd.Stdin = strings.NewReader("some input")
+	cmd := exec.Command("cmd","ver")
+	cmd.Stdin = strings.NewReader("some input")	
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
@@ -17,16 +18,25 @@ func GetInfo() *GoInfoObject {
 	err := cmd.Run()
 	if err != nil {							
 		panic(err)	
-	}
-	fmt.Println("Win:",out.String())	
+	}	
 	osStr := strings.Replace(out.String(),"\n","",-1)
 	osStr = strings.Replace(osStr,"\r\n","",-1)
-	osInfo := strings.Split(osStr," ")
+	tmp1 := strings.Index(osStr,"[Version")
+	tmp2 := strings.Index(osStr,"]")
+	var ver string
+	if tmp1 == -1 || tmp2 == -1 {
+		ver = "unknown"
+	} else {
+		ver = osStr[tmp1+9:tmp2]
+	}
+	
 	gio := new(GoInfoObject)
-	gio.Kernel = osInfo[0]
-	gio.Core = osInfo[1]
-	gio.Platform = osInfo[2]
-	gio.OS = osInfo[3]
+	gio.GoOS = runtime.GOOS
+	gio.Kernel = "windows"
+	gio.Core = ver
+	gio.Platform = "unknown"
+	gio.OS = "windows"
+	gio.CPUs = runtime.NumCPU()
 	gio.Hostname,_ = os.Hostname()	
 	return gio
 }
