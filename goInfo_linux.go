5c47c5df6f6e831044e67e2a047394ea1f7adc33
matishsiao <matismaya@gmail.com>
2014-06-09 16:23:19 +0800

    fix panic error

--- goInfo_linux.go
+++ goInfo_linux.go
@@ -6,9 +6,25 @@ import (
 	"os"
 	"bytes"
 	"runtime"
+	"fmt"
+	"time"
 )
 
 func GetInfo() *GoInfoObject {			
+	out := _getInfo()
+	for strings.Index(out,"broken pipe") != -1 {
+		out = _getInfo()
+		time.Sleep(500 * time.Millisecond)
+	}
+	osStr := strings.Replace(out,"\n","",-1)
+	osStr = strings.Replace(osStr,"\r\n","",-1)
+	osInfo := strings.Split(osStr," ")
+	gio := &GoInfoObject{Kernel:osInfo[0],Core:osInfo[1],Platform:osInfo[2],OS:osInfo[3],GoOS:runtime.GOOS,CPUs:runtime.NumCPU()}
+	gio.Hostname,_ = os.Hostname()	
+	return gio
+}
+
+func _getInfo() string {
 	cmd := exec.Command("uname","-srio")
 	cmd.Stdin = strings.NewReader("some input")
 	var out bytes.Buffer
@@ -17,12 +33,7 @@ func GetInfo() *GoInfoObject {
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {							
-		panic(err)	
+		fmt.Println("getInfo:",err)	
 	}
-	osStr := strings.Replace(out.String(),"\n","",-1)
-	osStr = strings.Replace(osStr,"\r\n","",-1)
-	osInfo := strings.Split(osStr," ")
-	gio := &GoInfoObject{Kernel:osInfo[0],Core:osInfo[1],Platform:osInfo[2],OS:osInfo[3],GoOS:runtime.GOOS,CPUs:runtime.NumCPU()}
-	gio.Hostname,_ = os.Hostname()	
-	return gio
+	return out.String()
 }
