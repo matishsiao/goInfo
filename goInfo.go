0a6237a36bfd094cd41dfb998714b8947d6ed9b3
ss23 <stephen@zxsecurity.co.nz>
2017-10-03 22:50:03 +1300

    Fix formatting using `go fmt`
    By running `go fmt` on the sourcetree, this ensures the entire package
    uses the consistent and standard go formatting.

--- goInfo.go
+++ goInfo.go
@@ -5,25 +5,25 @@ import (
 )
 
 type GoInfoObject struct {
-	GoOS string
-	Kernel string
-	Core string
+	GoOS     string
+	Kernel   string
+	Core     string
 	Platform string
-	OS string
+	OS       string
 	Hostname string
-	CPUs int
+	CPUs     int
 }
 
 func (gi *GoInfoObject) VarDump() {
-	fmt.Println("GoOS:",gi.GoOS)
-	fmt.Println("Kernel:",gi.Kernel)
-	fmt.Println("Core:",gi.Core)
-	fmt.Println("Platform:",gi.Platform)
-	fmt.Println("OS:",gi.OS)
-	fmt.Println("Hostname:",gi.Hostname)
-	fmt.Println("CPUs:",gi.CPUs)
+	fmt.Println("GoOS:", gi.GoOS)
+	fmt.Println("Kernel:", gi.Kernel)
+	fmt.Println("Core:", gi.Core)
+	fmt.Println("Platform:", gi.Platform)
+	fmt.Println("OS:", gi.OS)
+	fmt.Println("Hostname:", gi.Hostname)
+	fmt.Println("CPUs:", gi.CPUs)
 }
 
 func (gi *GoInfoObject) String() string {
-	return fmt.Sprintf("GoOS:%v,Kernel:%v,Core:%v,Platform:%v,OS:%v,Hostname:%v,CPUs:%v",gi.GoOS,gi.Kernel,gi.Core,gi.Platform,gi.OS,gi.Hostname,gi.CPUs)
+	return fmt.Sprintf("GoOS:%v,Kernel:%v,Core:%v,Platform:%v,OS:%v,Hostname:%v,CPUs:%v", gi.GoOS, gi.Kernel, gi.Core, gi.Platform, gi.OS, gi.Hostname, gi.CPUs)
 }
